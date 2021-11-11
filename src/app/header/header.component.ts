78a778a434c2032c41d765bb651eca3e0bd4d6b1
Paolo Racca <p.racca.1368@vallauri.edu>
2021-11-11 11:36:51 +0100

    [Components] Update components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  public open: boolean = false;
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
